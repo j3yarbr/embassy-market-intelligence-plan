daefabd63f29e6c0e5fa784d76ba710bdabfce8e
j3yarbr <matt@embassygear.com>
2026-07-02 11:53:51 -0500

    Delete cluster_callback.js

--- cluster_callback.js
+++
@@ -1,9 +0,0 @@
-function embassyCallback(row) {
-    return L.circleMarker([row[0], row[1]], {
-        radius: 12,
-        color: 'white',
-        weight: 2,
-        fillColor: row[3],
-        fillOpacity: 1
-    }).bindTooltip(row[2]);
-}
