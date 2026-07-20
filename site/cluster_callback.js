9f0befb30d05a527f951e6d5b429e8bbd8c816a6
Matt Yarbrough <matt@embassygear.com>
2026-07-20 09:54:16 -0500

    Update map: narrowed territory boundary, category filter, bulk marker loading

--- site/cluster_callback.js
+++ site/cluster_callback.js
@@ -1,9 +1,10 @@
 function embassyCallback(row) {
+    var tooltip = row[4] ? row[2] + " (" + row[4] + ")" : row[2];
     return L.circleMarker([row[0], row[1]], {
         radius: 12,
         color: 'white',
         weight: 2,
         fillColor: row[3],
         fillOpacity: 1
-    }).bindTooltip(row[2]);
+    }).bindTooltip(tooltip);
 }
